0b49708b030247afc805defebff85a09ea2d460b
fengzimu <2710780954@qq.com>
2017-05-14 15:21:01 +0800

    Demo first angular demo 2

---
+++ public/controllers/controller.js
@@ -0,0 +1,24 @@
+/*
+* @Author: YADONG
+* @Date:   2017-05-12 23:02:28
+<<<<<<< HEAD
+* @Last Modified by:   fengzimu
+* @Last Modified time: 2017-05-14 15:10:45
+*/
+
+'use strict';
+var app = angular.module("app", []);
+app.controller("AppCtrl", ['$scope', function($scope){
+	console.log("Say hi from controller");
+    $scope.say = "Hi, First node Demo";
+}]);
+=======
+* @Last Modified by:   YADONG
+* @Last Modified time: 2017-05-12 23:03:33
+*/
+
+'use strict';
+function appCtrl($scope){
+
+}
+>>>>>>> 5337bd2acc1b91958f8781f1502f74cf5c0f8c6e
