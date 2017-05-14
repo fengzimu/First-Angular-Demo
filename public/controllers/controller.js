48c6724c9cf711552b3c7cfd491991d856b778d6
fengzimu <2710780954@qq.com>
2017-05-14 15:36:54 +0800

    Change controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,7 +3,7 @@
 * @Date:   2017-05-12 23:02:28
 <<<<<<< HEAD
 * @Last Modified by:   fengzimu
-* @Last Modified time: 2017-05-14 15:10:45
+* @Last Modified time: 2017-05-14 15:36:01
 */
 
 'use strict';
@@ -12,13 +12,3 @@ app.controller("AppCtrl", ['$scope', function($scope){
 	console.log("Say hi from controller");
     $scope.say = "Hi, First node Demo";
 }]);
-=======
-* @Last Modified by:   YADONG
-* @Last Modified time: 2017-05-12 23:03:33
-*/
-
-'use strict';
-function appCtrl($scope){
-
-}
->>>>>>> 5337bd2acc1b91958f8781f1502f74cf5c0f8c6e
